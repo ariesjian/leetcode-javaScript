5108546592b0e3f9975097e90aec8c406dac3169
ariesjian <2890283899@qq.com>
2019-03-19 20:31:33 +0800

    格蕾编码答案提交存在疑问

--- code/array_learn/lesson4.js
+++ code/array_learn/lesson4.js
@@ -16,8 +16,8 @@ export const geliFun = (n) => {
             let result = [];
             let max = Math.pow(2, n) - 1; // 为了找对称线  -1是为了找到下标  长度-1就是下标
             for (let i = 0, len = prev.length; i < len; i++) { //  不需要关注顺序
-                result[i] = `0${prev[i]}`; // 将低位前面加0
-                result[max - i] = `1${prev[i]}`; // 将高位前面加1
+                result[i] = parseInt(`0${prev[i]}`, 2); // 将低位前面加0
+                result[max - i] = parseInt(`1${prev[i]}`, 2); // 将高位前面加1
             }
             return result
         }
@@ -25,3 +25,23 @@ export const geliFun = (n) => {
     return make(n)
 };
 
+const grayCode = (n) => {
+    // 递归函数，用来算输入为n的格雷编码序列
+    let make = n => {
+        if (n === 1) { // 1单独处理
+            return ['0', '1']
+        } else {
+            let prev = make(n - 1); // 拿到n-1之前的输出 为了找对称低位值
+            let result = [];
+            let max = Math.pow(2, n) - 1; // 为了找对称线  -1是为了找到下标  长度-1就是下标
+            for (let i = 0, len = prev.length; i < len; i++) { //  不需要关注顺序
+                result[i] = `0${prev[i]}`; // 将低位前面加0
+                result[max - i] = `1${prev[i]}`; // 将高位前面加1
+            }
+            return result
+        }
+    };
+    return make(n).map(i => {
+        return parseInt(i, 2)
+    });
+};
