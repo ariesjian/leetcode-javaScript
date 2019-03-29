2aba1f55dbf7da42ed8141760cc04fec82b118a7
ariesjian <2890283899@qq.com>
2019-03-29 13:54:14 +0800

    格式化

--- code/sort_learn/lesson1.js
+++ code/sort_learn/lesson1.js
@@ -11,14 +11,14 @@ export default (arr) => {
   let odd = 1;
   let even = 0;
   // 对数组进行遍历
-  arr.forEach(item => {
-    if (item % 2 === 1) {
-      r[odd] = item;
-      odd += 2
-    } else {
-      r[even] = item;
-      even += 2
-    }
-  });
+    arr.forEach(item => {
+        if (item % 2 === 1) {
+            r[odd] = item;
+            odd += 2
+        } else {
+            r[even] = item;
+            even += 2
+        }
+    });
   return r
 }
