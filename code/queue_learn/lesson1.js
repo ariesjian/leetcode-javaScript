ab312d4250468fb7704c7db77667bb8c9cfa3ef8
ariesjian <2890283899@qq.com>
2019-04-11 13:10:35 +0800

    循环队列的定义

--- code/queue_learn/lesson1.js
+++ code/queue_learn/lesson1.js
@@ -5,14 +5,16 @@
 *  题目要求
 *  MyCircularQueue(k): 构造器，设置队列长度为 k 。
 *  Front: 从队首获取元素。如果队列为空，返回 -1 。
-* Rear: 获取队尾元素。如果队列为空，返回 -1 。
+*  Rear: 获取队尾元素。如果队列为空，返回 -1 。
 * enQueue(value): 向循环队列插入一个元素。如果成功插入则返回真。
 * deQueue(): 从循环队列中删除一个元素。如果成功删除则返回真。
 * isEmpty(): 检查循环队列是否为空。
 * isFull(): 检查循环队列是否已满。
+*  todo  这个代码在leetcode上跑不通  需要老师帮忙看看
 * */
 export default class MyCircularQueue {
-    constructor (k) {
+    // 构造器，设置队列长度为 k 。
+    constructor(k) {
         // 用来保存数据长度为k的数据结构
         this.list = Array(k);
         // 队首指针
@@ -23,37 +25,55 @@ export default class MyCircularQueue {
         this.max = k
     }
 
-    enQueue (num) {
-        if (this.isFull()) {
+// 向循环队列插入一个元素。如果成功插入则返回真。
+    enQueue(num) {
+        if (this.isFull() || this.list[this.rear]) { // 判断尾指针是不是满了
             return false
         } else {
             this.list[this.rear] = num;
-            this.rear = (this.rear + 1) % this.max;
+            this.rear = (this.rear + 1) % this.max;// 循环队列  如果末尾插满了 那么尾指针要移动到队首
             return true
         }
     }
 
-    deQueue () {
-        let v = this.list[this.front];
-        this.list[this.front] = '';
-        this.front = (this.front + 1) % this.max;
-        return v
+// 从循环队列中删除一个元素。如果成功删除则返回真。
+    deQueue() {
+        let v = this.list[this.front]; // 获取队首的元素
+        this.list[this.front] = '';// 让队首的元素为空 删除该元素
+        this.front = (this.front + 1) % this.max;// 不要忘记移动队首的指针
+        return v ? v : false
     }
 
-    isEmpty () {
-        return this.front === this.rear && !this.list[this.front]
+// 队列为空
+    isEmpty() {
+        if (!this.list[this.front] && !this.list[this.rear]) {
+            return true
+        } else {
+            return false
+        }
+     // return this.front === this.rear && !this.list[this.front] // 队首指针元素等于队尾指针且对手元素为空
     }
 
-    isFull () {
-        return this.front === this.rear && !!this.list[this.front]
+// 队列未满
+    isFull() {
+        return this.front === this.rear && !!this.list[this.front] // 队首指针元素等于队尾指针且对手元素不为空
     }
 
-    Front () {
-        return this.list[this.front]
+// 从队首获取元素。如果队列为空，返回 -1 。
+    Front() {
+        let v = this.list[this.front]; // 获取队首的元素
+        return v ? this.list[this.front] : -1
+        // return this.list[this.front]
     }
 
-    Rear () {
-        let rear = this.rear - 1;
-        return this.list[rear < 0 ? this.max - 1 : rear]
+// 获取队尾元素。如果队列为空，返回 -1 。
+    Rear() {
+        // let rear = this.rear - 1;
+        // return this.list[rear < 0 ? this.max - 1 : rear]
+      if(this.isEmpty()){
+          return -1
+      }else{
+          return this.list[this.rear]
+      }
     }
 }
